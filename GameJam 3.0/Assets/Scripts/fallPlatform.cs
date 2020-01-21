231f8977255034056aeaa23ae40906cbb096fac9
Adria-F <adriaferrer2898@gmail.com>
2020-01-21 17:54:38 +0100

    falling platforms ;)

---
+++ GameJam 3.0/Assets/Scripts/fallPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallPlatform : MonoBehaviour
+{
+    public float fallTime;
+    public float fallSpeed;
+
+    bool countdown = false;
+    bool falling = false;
+    float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!falling && countdown && Time.time - time >= fallTime)
+        {
+            transform.LeanMoveY(transform.position.y - 2000, 2000 / fallSpeed);
+            falling = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Coolide");
+        if (!countdown && other.collider.tag == "Player")
+        {
+            countdown = true;
+            time = Time.time;
+        }
+        if (falling && other.collider.tag == "Obstacle")
+        {
+            Debug.Log("Wall");
+            Destroy(gameObject);
+        }
+    }
+}
--- GameJam 3.0/Assets/Scripts/movePlatform.cs
+++ GameJam 3.0/Assets/Scripts/movePlatform.cs
@@ -13,18 +13,19 @@ public class movePlatform : MonoBehaviour
     public movingAxis axis;
 
     public float speed;
+    public float distance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 direction = (axis == movingAxis.HORITZONTAL) ? Vector2.right : Vector2.up;
+
+        transform.LeanMove(new Vector2(transform.position.x, transform.position.y) + direction * distance, Mathf.Abs(distance/speed)).setLoopPingPong();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 direction = (axis == movingAxis.HORITZONTAL)? Vector2.right : Vector2.up;
-
-        transform.Translate(direction * speed * Time.deltaTime);
+        
     }
 }
