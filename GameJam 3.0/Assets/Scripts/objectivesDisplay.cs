0fb39f4aad0620fc515304880024d0d114a6710f
Adria-F <adriaferrer2898@gmail.com>
2020-01-21 19:42:15 +0100

    implemented objective bar

---
+++ GameJam 3.0/Assets/Scripts/objectivesDisplay.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectivesDisplay : MonoBehaviour
+{
+    public float barHeight = 200.0f;
+    public float margins = 2.0f;
+
+    public GameObject step;
+    public bool isPreview = false;
+
+    public enum Objectives
+    {
+        GREEN,
+        YELLOW,
+        RED
+    };
+    public Objectives[] objectives;
+
+    public colorTemplate green;
+    public colorTemplate yellow;
+    public colorTemplate red;
+
+    float stepheight = 0.0f;
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = objectives.Length;
+        stepheight = (barHeight - (num - 1) * margins) / num;
+
+        if (isPreview)
+        {
+            for(int i = 0; i < num; i++)
+            {
+                addStep(objectives[i]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addStep(Objectives color)
+    {
+        GameObject go = Instantiate(step, transform);
+
+        go.GetComponent<RectTransform>().localPosition = new Vector3(0.0f, ((stepheight + margins) * count + stepheight / 2));
+        go.GetComponent<RectTransform>().sizeDelta = new Vector2(25.0f, stepheight);
+
+        switch(color)
+        {
+            case Objectives.YELLOW:
+                go.GetComponent<Image>().color = yellow.value;
+                break;
+            case Objectives.GREEN:
+                go.GetComponent<Image>().color = green.value;
+                break;
+            case Objectives.RED:
+                go.GetComponent<Image>().color = red.value;
+                break;
+        }
+        
+
+        count++;
+    }
+}
