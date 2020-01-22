e73889928f31b70b8696d2002130b144cecd3973
Skyway666 <lucasgarciamateu@gmail.com>
2020-01-22 09:28:24 +0100

    Main menu and credits scene update

---
+++ GameJam 3.0/Assets/Scripts/Scene Navigation/MusicChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnStartGame()
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
