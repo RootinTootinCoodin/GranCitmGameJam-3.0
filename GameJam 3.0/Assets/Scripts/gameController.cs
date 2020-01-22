1cc599b0ef271b984b4f98acc3beef15ff4dd35a
Adria-F <adriaferrer2898@gmail.com>
2020-01-22 01:10:14 +0100

    updated levels

--- GameJam 3.0/Assets/Scripts/gameController.cs
+++ GameJam 3.0/Assets/Scripts/gameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameController : MonoBehaviour
 {
@@ -27,6 +28,7 @@ public class gameController : MonoBehaviour
         {
             winMenu.SetActive(true);
             finished = true;
+            StartCoroutine(loadNextScene());
         }
     }
 
@@ -36,6 +38,31 @@ public class gameController : MonoBehaviour
         {
             loseMenu.SetActive(true);
             finished = true;
+            StartCoroutine(loadCurrentScene());
         }
+    }
+
+    public void reloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void nextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        nextScene();
+    }
+
+    public IEnumerator loadCurrentScene()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        reloadScene();
     }
 }
